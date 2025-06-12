c3e3a463c14b94fb82d578b3a5b8a8899e7890be
Olga Yang <ya1994ng@gmail.com>
2025-06-12 14:31:31 +0800

    feat: add toggle for disable sandbox and introspection

--- src/index.ts
+++ src/index.ts
@@ -29,56 +29,58 @@ const typeDefs = gql(readFileSync("./schema.graphql", "utf8"));
 const schema = makeExecutableSchema({ typeDefs, resolvers });
 
 
+const plugins = [
+  useDataLoader('users', () => new DataLoader(userRepo.batchGetUsersById)),
+  useDataLoader('posts', () => new DataLoader(postRepo.batchGetPostsByAuthorId)),
+  useDataLoader('post', () => new DataLoader(postRepo.batchGetPostById)),
+  useDataLoader('comments', () => new DataLoader(commentRepo.batchGetCommentsByPostId)),
+  usePrometheus({
+    endpoint: '/metrics', // optional, default is `/metrics`, you can disable it by setting it to `false` if registry is configured in "push" mode
+    // Optional, see default values below
+    metrics: {
+      // By default, these are the metrics that are enabled:
+      graphql_envelop_request_time_summary: true,
+      graphql_envelop_phase_parse: true,
+      graphql_envelop_phase_validate: true,
+      graphql_envelop_phase_context: true,
+      graphql_envelop_phase_execute: true,
+      graphql_envelop_phase_subscribe: true,
+      graphql_envelop_error_result: true,
+      graphql_envelop_deprecated_field: true,
+      graphql_envelop_request_duration: true,
+      graphql_envelop_schema_change: true,
+      graphql_envelop_request: true,
+      graphql_yoga_http_duration: true,
+
+      // This metric is disabled by default.
+      // Warning: enabling resolvers level metrics will introduce significant overhead
+      graphql_envelop_execute_resolver: true
+    },
+
+  }),
+  useEnvelop(envelop({
+    plugins: [
+      useValidationRule(
+        depthLimit({
+          maxDepth: 6,
+        })
+      )
+    ]
+  })),
+];
+
+if (process.env.NODE_ENV === 'production') {
+  plugins.push(useDisableIntrospection());
+}
 
 const yoga = createYoga({
   schema,
   context: () => ({
     pubSub,
   }),
-  plugins: [
-    useDataLoader('users', () => new DataLoader(userRepo.batchGetUsersById)),
-    useDataLoader('posts', () => new DataLoader(postRepo.batchGetPostsByAuthorId)),
-    useDataLoader('post', () => new DataLoader(postRepo.batchGetPostById)),
-    useDataLoader('comments', () => new DataLoader(commentRepo.batchGetCommentsByPostId)),
-    usePrometheus({
-      endpoint: '/metrics', // optional, default is `/metrics`, you can disable it by setting it to `false` if registry is configured in "push" mode
-      // Optional, see default values below
-      metrics: {
-        // By default, these are the metrics that are enabled:
-        graphql_envelop_request_time_summary: true,
-        graphql_envelop_phase_parse: true,
-        graphql_envelop_phase_validate: true,
-        graphql_envelop_phase_context: true,
-        graphql_envelop_phase_execute: true,
-        graphql_envelop_phase_subscribe: true,
-        graphql_envelop_error_result: true,
-        graphql_envelop_deprecated_field: true,
-        graphql_envelop_request_duration: true,
-        graphql_envelop_schema_change: true,
-        graphql_envelop_request: true,
-        graphql_yoga_http_duration: true,
-
-        // This metric is disabled by default.
-        // Warning: enabling resolvers level metrics will introduce significant overhead
-        graphql_envelop_execute_resolver: true
-      },
-
-    }),
-    useEnvelop(envelop({
-      plugins: [
-        useValidationRule(
-          depthLimit({
-            maxDepth: 6,
-          })
-        )
-      ]
-    })),
-  ]
-  // graphiql: false,
-  // plugins: [useDisableIntrospection()]
+  plugins: plugins,
+  graphiql: process.env.NODE_ENV === 'production',
 });
-
-
 
 const server = createServer(yoga);
 const wsServer = new WebSocketServer({
